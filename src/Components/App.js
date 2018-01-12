053b68e43d3d6ba27bca22bb49ad83f1e825716a
Casey Jordan <Jordan_Casey.bah.com>
2018-01-12 11:12:55 -0500

    fixing surge issue

--- src/Components/App.js
+++ src/Components/App.js
@@ -60,10 +60,14 @@ class App extends Component {
   this.getRec = this.getRec.bind(this)
   this.setLocation = this.setLocation.bind(this)
   this.seeResults = this.seeResults.bind(this)
+  this.retakeQuiz = this.retakeQuiz.bind(this)
 }
 
   retakeQuiz(){
-    window.location.reload()
+    this.setState({
+      class: "btn-hide",
+      class2: "getRec"
+    })
   }
 
   seeResults(){
